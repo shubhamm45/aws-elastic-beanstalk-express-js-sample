01f0fab15fbff4d4cd12508ad7c1745ea749cd60
shubhamm45 <60311835+shubhamm45@users.noreply.github.com>
2020-12-29 09:42:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Shubham - you have created dev-ops pipeline!'));
+app.get('/', (req, res) => res.send('Hello Shubham - you have created a dev-ops pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
